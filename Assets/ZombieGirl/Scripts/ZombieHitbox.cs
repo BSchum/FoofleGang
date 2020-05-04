21385185e0baa22ab963fb204e4737d8ccdf8c69
ThibaudBOURGEAT <thibaud.bourgeat.trx@gmail.com>
2020-05-04 15:26:49 +0200

    Hitbox done

---
+++ Assets/ZombieGirl/Scripts/ZombieHitbox.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FoofleGang.Enemies
+{
+    public class ZombieHitbox : MonoBehaviour, IDamageable
+    {
+        [SerializeField] private float _damageMultiplicater;
+        private ZombieController _zombie;
+
+        void Start()
+        {
+            _zombie = GetComponentInParent<ZombieController>();
+        }
+
+        public void TakeDamage(float damage)
+        {
+            _zombie.TakeDamage(damage * _damageMultiplicater);
+        }
+    }
+}
