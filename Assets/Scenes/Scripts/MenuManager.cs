eb3aa97a72c6c6e77f10235c228be61126bdc533
Pierre Plessy <pierr.plessy@outlook.fr>
2020-05-04 19:37:15 +0200

    new menu manager

---
+++ Assets/Scenes/Scripts/MenuManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public string launchScene;
+    public void LaunchScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(launchScene, LoadSceneMode.Single);
+    }
+
+    public void LeaveApplication()
+    {
+        Application.Quit();
+    }
+}
